5802a77af6973dadd5665a93d776f1556ef14002
carter-nguyen-goldenowl <carter.nguyen.goldenowl@gmail.com>
2022-05-30 11:20:37 +0700

    Base-API

---
+++ config/services.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'google' => [
+        'client_id'     => env('622622571724-6i6udqn1pfdl0ajkokt9d6utgae050bv.apps.googleusercontent.com'),
+        'client_secret' => env('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'),
+        'redirect'      => env('https://5ae3-113-161-66-128.ap.ngrok.io'),
+    ],
+
+];
